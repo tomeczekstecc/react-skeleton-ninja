727cad642b9d5a44ad22bb029a2040e51922b113
Tomasz Steć <tomeczekstecc@gmail.com>
2021-01-11 21:18:53 +0100

    Lesson 1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import userEvent from '@testing-library/user-event';
+import Articles from './components/Articles';
+import User from './components/User';
+
+function App() {
+  return (
+    <div className='App'>
+      <header>
+        <h1>React Skeletons</h1>
+      </header>
+      <div className='content'>
+        <Articles/>
+        <User />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const user = () => {
+  return (
+    <div className='user'>
+      <h2>User details</h2>
+    </div>
+  );
+};
+
+export default user;
